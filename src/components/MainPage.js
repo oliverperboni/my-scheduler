3132273550b5d69c53a774c090c73cbd0df745ab
Oliver <oliverperboni@gmail.com>
2024-03-18 16:20:22 +0000

    the user can select the time now

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,8 +5,9 @@ import Scheduler from "./Scheduler";
 
 function MainPage() {
   const [escolha, setEscolha] = useState(true);
-
-
+  const [data, setData] = useState("")
+ 
+  
   function nextOp(){
     setEscolha(!escolha)
   }
@@ -20,12 +21,15 @@ function MainPage() {
             </h2>
         ) : (
         <div>
-            <Scheduler horaInicio="08:00" horaFim="13:30" intervalo="30" horaInicioTarde="16:00" horaFimTarde="22:00"/>
+            <Scheduler horaInicio="08:00" horaFim="13:30" intervalo="30" horaInicioTarde="16:00" horaFimTarde="22:00" setData={setData} />
             Foi escolhido os serviço
         </div>
         )
 
         }
+        <div>
+          {data}
+        </div>
         <button onClick={nextOp}>Mostrar Horarios </button>
         
       </div>
--- src/components/Scheduler.js
+++ src/components/Scheduler.js
@@ -1,73 +1,85 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-
-
+import React from "react";
+import { useEffect, useState } from "react";
 
 function Scheduler(props) {
-    const [startTime, setStartTime] = useState(props.horaInicio);
-    const [endTime, setEndTime] = useState(props.horaFim);
-    const [interval, setInterval] = useState(props.intervalo);
-    const [startTimeAfter, setStartTimeAfter] = useState(props.horaInicioTarde);
-    const [endTimeAfter, setEndTimeAfter] = useState(props.horaFimTarde);
-    const [result, setResult] = useState([]);
-    const [resultAfternoon, setResultAfternoon] = useState([]);
-    useEffect( () => {
-      generateTimeRangeToMorning()
-      generateTimeRangeToAfternoon()
+  const [startTime, setStartTime] = useState(props.horaInicio);
+  const [endTime, setEndTime] = useState(props.horaFim);
+  const [interval, setInterval] = useState(props.intervalo);
+  const [startTimeAfter, setStartTimeAfter] = useState(props.horaInicioTarde);
+  const [endTimeAfter, setEndTimeAfter] = useState(props.horaFimTarde);
+  const [result, setResult] = useState([]);
+  const [resultAfternoon, setResultAfternoon] = useState([]);
+  useEffect(() => {
+    generateTimeRangeToMorning();
+    generateTimeRangeToAfternoon();
+    
+  }, []);
 
-    },[])
+  const generateTimeRangeToAfternoon = () => {
+    const startAfter = new Date(`2024-01-01T${startTimeAfter}`);
+    const endAfter = new Date(`2024-01-01T${endTimeAfter}`);
+    const intervalValueAfter = parseInt(interval, 10);
+    const timeRange = [];
+    let currentTime = startAfter;
 
-    const generateTimeRangeToAfternoon = () => {
-      const startAfter = new Date(`2024-01-01T${startTimeAfter}`);
-      const endAfter = new Date(`2024-01-01T${endTimeAfter}`);
-      const intervalValueAfter = parseInt(interval, 10);
-      const timeRange = [];
-      let currentTime = startAfter;
-  
-      while (currentTime <= endAfter) {
-        timeRange.push(currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-        currentTime.setMinutes(currentTime.getMinutes() + intervalValueAfter);
-      }
- 
-      setResultAfternoon(timeRange);
-    };
-  
-    const generateTimeRangeToMorning = () => {
-      const start = new Date(`2024-01-01T${startTime}`);
-      const end = new Date(`2024-01-01T${endTime}`);
-      const intervalValue = parseInt(interval, 10);
-      console.log("Na manhã o horaio é: "+props.horaFim)
-      const timeRange = [];
-      let currentTime = start;
-  
-      while (currentTime <= end) {
-        timeRange.push(currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-        currentTime.setMinutes(currentTime.getMinutes() + intervalValue);
-      }
-  
-      setResult(timeRange);
-    };
-  
-    return (
+    while (currentTime <= endAfter) {
+      timeRange.push(
+        currentTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+      currentTime.setMinutes(currentTime.getMinutes() + intervalValueAfter);
+    }
+
+    setResultAfternoon(timeRange);
+  };
+
+  const generateTimeRangeToMorning = () => {
+    const start = new Date(`2024-01-01T${startTime}`);
+    const end = new Date(`2024-01-01T${endTime}`);
+    const intervalValue = parseInt(interval, 10);
+    console.log("Na manhã o horaio é: " + props.horaFim);
+    const timeRange = [];
+    let currentTime = start;
+
+    while (currentTime <= end) {
+      timeRange.push(
+        currentTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+      currentTime.setMinutes(currentTime.getMinutes() + intervalValue);
+    }
+
+    setResult(timeRange);
+  };
+
+  return (
+    <div>
       <div>
-        <div>
-          <h2>Resultado da manhã:</h2>
-          <ul>
-            {result.map((time, index) => (
-              <li key={index}>{time}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h2>Resultado da tarde:</h2>
-          <ul>
-            {resultAfternoon.map((time, index) => (
-              <li key={index}>{time}</li>
-            ))}
-          </ul>
-        </div>
+        <h2>Resultado da manhã:</h2>
+        <ul>
+          {result.map((time, index) => (
+            <li key={index}>
+              <button onClick={() => props.setData(time)}>{time}</button>
+            </li>
+          ))}
+        </ul>
       </div>
-    );
+      <div>
+        <h2>Resultado da tarde:</h2>
+        <ul>
+          {resultAfternoon.map((time, index) => (
+            <li key={index}>
+              <button onClick={() => props.setData(time)}>{time}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Scheduler
+export default Scheduler;
