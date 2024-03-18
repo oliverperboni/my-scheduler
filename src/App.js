45d68b68808d9a13fe3a569527275023e94e0e9d
oliverperboni <115580335+oliverperboni@users.noreply.github.com>
2024-03-18 10:08:15 +0000

    conditional rendering start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Scheduler from "./components/Scheduler";
+import MainPage from "./components/MainPage";
 function App() {
   return (
-   <Scheduler horaInicio="08:00" horaFim="13:30" intervalo="30" horaInicioTarde="16:00" horaFimTarde="22:00"/>
+  //  <Scheduler horaInicio="08:00" horaFim="13:30" intervalo="30" horaInicioTarde="16:00" horaFimTarde="22:00"/>
+    <MainPage/>
   );
 }
 
---
+++ src/components/MainPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import Scheduler from "./Scheduler";
+
+
+function MainPage() {
+  const [escolha, setEscolha] = useState(true);
+
+
+  function nextOp(){
+    setEscolha(!escolha)
+  }
+  return (
+    <>
+      <div>
+      <h2>MainPage</h2>
+        {escolha  ? (
+            <h2>
+                Escolha os serviço
+            </h2>
+        ) : (
+        <div>
+            <Scheduler horaInicio="08:00" horaFim="13:30" intervalo="30" horaInicioTarde="16:00" horaFimTarde="22:00"/>
+            Foi escolhido os serviço
+        </div>
+        )
+
+        }
+        <button onClick={nextOp}>Mostrar Horarios </button>
+        
+      </div>
+    </>
+  );
+}
+
+export default MainPage;
